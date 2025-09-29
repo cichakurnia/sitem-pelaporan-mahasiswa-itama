5d58bd8fd86b6efc8cd9b9a7fa8b0da89ffc319d
cicha <cichadewi112@gmail.com>
2025-09-29 10:26:28 +0700

    membuat migrasi

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Sistem Pelaporan Masalah - Teknik Informatika";
 });
+Route::get('/daftar-masalah', function () {
+  return "daftar-masalah";
+    });
